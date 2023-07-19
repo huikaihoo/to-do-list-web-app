41bb528cee3a12a819d966dd6b49cfd5a562e373
Simon Hui <huikaihoo@gmail.com>
2023-07-20 01:03:23 +0800

    Init project structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # to-do-list-web-app
+
+## Init project
+
+```
+nvm use
+yarn set version stable
+yarn config set nodeLinker node-modules
+yarn config set enableGlobalCache true
+yarn
+```
---
+++ server/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001, '0.0.0.0');
+}
+bootstrap();
